63bc30899655f2a71e9e60f09d1c36b10c6a34a6
Nikita Bhoyar <132975338+NikitaBhoyar935020@users.noreply.github.com>
2023-06-27 12:01:35 +0530

    Update BookController.cs

--- Controllers/BookController.cs
+++ Controllers/BookController.cs
@@ -5,16 +5,17 @@ using System.Net;
 
 namespace BookStore.Controllers
 {
+   ///This is the router
     [Route("api/[controller]")]
     [ApiController]
     public class BookController : ControllerBase
     {
-        static List<Book> books = new List<Book>();
+        static List<Book> books = new List<Book>();     ///This is the list whoch stored books.
 
         [HttpGet]
         public IEnumerable<Book> Get()
         {
-            return books;
+            return books;                              ///It returns books details.
         }
 
         //GET api/<BookController>
@@ -25,10 +26,10 @@ namespace BookStore.Controllers
 
             if (book == null)
             {
-                return NotFound();
+                return NotFound();                  // Return 404 Not Found if the book is not found
             }
 
-            return Ok(book);
+            return Ok(book);                       // Return 200 OK if the book is successfully fetch.
 
         }
 
@@ -39,16 +40,16 @@ namespace BookStore.Controllers
             
             if (books.Any(x => x.ISBN == book.ISBN))
             {
-                return BadRequest("Book ISBN is already exits!!");
+                return BadRequest("Book ISBN is already exits!!");  // Return 400 OK if the book is successfully not submitted.
             }
             books.Add(book);
-            return Ok();
+            return Ok();                                           // Return 200 OK if the book is successfully submitted.
         }
 
 
         // PUT api/<StudentController>
-        [HttpPut("{isbn}")]    
-        public ActionResult<Book> Update(string isbn, Book updatedBook)
+        [HttpPut("{isbn}")]                                             
+        public ActionResult<Book> Update(string isbn, Book updatedBook)  //This method updated the book details.
         {
             var book = books.FirstOrDefault(x => x.ISBN == isbn);
 
@@ -67,19 +68,19 @@ namespace BookStore.Controllers
         }
         //Delete api/<DeleteController>
         [HttpDelete]
-        public ActionResult DeleteBook(string title, string isbn)
+        public ActionResult DeleteBook(string title, string isbn)         //This method delete the book details.       
         {
                 var book = books.FirstOrDefault(x => x.Title == title && x.ISBN == isbn);
 
                 if (book == null)
                 {
-                    return NotFound(); // Return 404 Not Found if the book is not found
+                    return NotFound();                                  // Return 404 Not Found if the book is not found
                 }
 
                 books.Remove(book);
                // books.SaveChanges();
 
-                return Ok(); // Return 200 OK if the book is successfully deleted
+                return Ok();                                            // Return 200 OK if the book is successfully deleted
            
         }
 
